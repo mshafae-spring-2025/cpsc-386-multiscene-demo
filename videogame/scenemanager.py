131a65ca6ec62128df43891ba1e0d9c69b70abf2
Michael Shafae <mshafae@fullerton.edu>
2025-02-26 14:22:12 -0800

    Removed unused code.

--- videogame/scenemanager.py
+++ videogame/scenemanager.py
@@ -7,8 +7,6 @@ class SceneManager:
     def __init__(self, scenes_list=None):
         """Initialize a scene manager with a given list of scenes."""
         self._scenes = scenes_list
-        self._current_scene = None
-        self._next_scene = None
 
     def __iter__(self):
         """Return an iterator to move through the scenes."""
